2400deb41724e52962932697aedd063501bdb958
Jonathan Lundström <contact@jonathanlundstrom.me>
2018-05-27 14:17:24 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,27 @@
+# Conrad Assistant
+
+This project aims to help you create your own Google Home through the use of a Raspberry Pi, an Arduino Nano and some cheap components such as a USB soundcard, speaker and microphone. The project was developed in partnership with [Conrad](https://www.conrad.se/) in their efforts to showcase makers around Sweden. I would therefore like to thank them for providing the hardware used in this project. Please also check out [their website](http://tekkie.se/) for more interesting projects. I would also like to thank Eric Vinjegaard for his work on the 3D-printed chassi and Jirka van der Roest for his work on the Arduino code.
+
+### Usage
+Clone the repository to a folder of your liking and use the following command to start the software:
+
+    python assistant.py
+    
+### Build guide
+Please refer to this link for a complete description on how and where to use this software.
+
+[http://jonathanlundstrom.me/2018/05/27/project-conrad-assistant/](http://jonathanlundstrom.me/2018/05/27/project-conrad-assistant/)
+
+### Supervisor job
+If you wish to have the software running all the time, you can use Supervisor to accomplish this. This is the configuration file that I use. It should be placed in `/etc/supervisor/conf.d` and named `sensor.conf` or similar.
+
+    [program:sensor]
+    directory=/home/pi/Applications/ConradAssistant
+    command=python assistant.py
+    autostart=true
+    autorestart=true
+    stderr_logfile=/var/log/sensor.err.log
+    stdout_logfile=/var/log/sensor.out.log
+
+### Finished product
+This section will in time be updated with a video of the final build and product.
---
+++ assistant.py
@@ -0,0 +1,76 @@
+#!/usr/bin/env python
+#
+# assistant.py
+# Custom Google Home based on the Google Assistant SDK for Python.
+# Use this script as is or modify it to suit your requirements.
+#
+# Date:      27/05/2018
+# Author:    Jonathan Lundstrom
+# Website:   http://jonathanlundstrom.me
+# Email:     contact@jonathanlundstrom.me
+
+import time
+import json
+import struct
+import serial
+import os.path
+import argparse
+import RPi.GPIO as GPIO
+
+import google.oauth2.credentials
+from google.assistant.library import Assistant
+from google.assistant.library.event import EventType
+from google.assistant.library.file_helpers import existing_file
+
+# Serial setup:
+ring = serial.Serial('/dev/ttyUSB0', 115200, timeout=1);
+time.sleep(2)
+ring.write(b'0')
+
+# GPIO setup:
+GPIO.setmode(GPIO.BCM)
+GPIO.setwarnings(False)
+GPIO.setup(25, GPIO.OUT)
+
+def process_event(event):
+    if event.type == EventType.ON_START_FINISHED:
+        print()
+        ring.write(b'1')
+
+    if event.type == EventType.ON_CONVERSATION_TURN_STARTED:
+        print()
+        ring.write(b'2')
+        GPIO.output(25,True)
+
+    print(event)
+
+    if event.type == EventType.ON_NO_RESPONSE:
+        print()
+
+    if event.type == EventType.ON_RESPONDING_STARTED:
+        print()
+        ring.write(b'3')
+
+    if event.type == EventType.ON_RESPONDING_FINISHED:
+        print()
+
+    if (event.type == EventType.ON_CONVERSATION_TURN_FINISHED and event.args and not event.args['with_follow_on_turn']):
+        print()
+        ring.write(b'4')
+        GPIO.output(25,False)
+
+def initialize():
+
+    parser = argparse.ArgumentParser(formatter_class=argparse.RawTextHelpFormatter)
+    parser.add_argument('--credentials', type=existing_file, metavar='OAUTH2_CREDENTIALS_FILE', default=os.path.join(os.path.expanduser('/home/pi/.config'), 'google-oauthlib-tool', 'credentials.json'), help='Path to store and read OAuth2 credentials')
+    args = parser.parse_args()
+    with open(args.credentials, 'r') as f:
+        credentials = google.oauth2.credentials.Credentials(token=None, **json.load(f))
+
+    with Assistant(credentials) as assistant:
+        for event in assistant.start():
+            process_event(event)
+
+
+if __name__ == '__main__':
+    initialize()
